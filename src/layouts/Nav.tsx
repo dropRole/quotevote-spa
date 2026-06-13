f7535a34e9312649532671f1c0cdbf505e3e72a3
dropRole <dusan.radosavljevic82@gmail.com>
2026-06-13 17:52:11 +0200

    Added nav items when user is logged in

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,20 +1,26 @@
-import { useState, type FC, type JSX } from "react";
+import { useContext, useState, type FC, type JSX } from "react";
 import "./nav.css";
 import { useLocation, useNavigate } from "react-router";
 import Button from "../components/Button";
 import Hamburger from "../components/Hamburger";
+import UserContext from "../contexts/UserContext";
+import defaultAvatar from "../assets/icons/default-avatar.png";
 
 const Nav: FC = () => {
   const [opened, setOpened] = useState<boolean>(false);
 
   const location = useLocation();
 
+  const isLoggedInUser = localStorage.getItem("quotevote-session");
+
+  const userContext = useContext(UserContext);
+
   const navigate = useNavigate();
 
   const renderNavItems = () => {
     const items: JSX.Element[] = [];
 
-    if (location.pathname !== "/login")
+    if (location.pathname !== "/login" && !isLoggedInUser)
       items.push(
         <Button
           key="loginbtn"
@@ -25,7 +31,7 @@ const Nav: FC = () => {
         />,
       );
 
-    if (location.pathname !== "/signup")
+    if (location.pathname !== "/signup" && !isLoggedInUser)
       items.push(
         <Button
           key="signupbtn"
@@ -35,6 +41,22 @@ const Nav: FC = () => {
           text="Signup"
         />,
       );
+
+    if (location.pathname === "/" && isLoggedInUser)
+      items.push([
+        <span key="settings-item">Settings</span>,
+        <span key="logout-item">Logout</span>,
+        <div
+          key="avatar-item"
+          data-user-fullname={`${userContext?.name} ${userContext?.surname}`}
+        >
+          <img src={userContext?.avatar ?? defaultAvatar} alt="user avatar" />
+        </div>,
+        <div key="plus-item">
+          <span></span>
+          <span></span>
+        </div>,
+      ]);
 
     return items.map((item) => item);
   };
@@ -46,6 +68,10 @@ const Nav: FC = () => {
         <p onClick={() => navigate("/")}>
           Quote<span>Vote</span>
         </p>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
       </div>
       <div>{renderNavItems()}</div>
     </nav>
